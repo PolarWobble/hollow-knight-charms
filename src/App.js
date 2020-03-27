acb7fb90bf8d71ff159a6991db2cfa31bf354665
polarwobble <johan.fors.90@gmail.com>
2020-03-27 14:55:15 +0100

    First commit, base

--- src/App.js
+++ src/App.js
@@ -3,24 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  	return (
+    	<div>
+      		<body className="App-header">
+      			<div>
+	      			<div class="ba b--light-silver br4 pv3 ph4">
+	      				<div>
+	      					<text class="f3 lh-copy">Map location</text><br/>
+	      					<text class="f5 lh-copy">click image of charm</text>
+	      				</div>
+	      				<br/>
+	      				<div>
+	      					<text class="f3 lh-copy">Wiki link</text><br/>
+	      					<text class="f5 lh-copy">click name of charm</text>
+	      				</div>
+	      			</div>
+
+	      			<br/><br/><br/><br/><br/><br/>
+
+	      			<div className="Top-row">
+	      				<a href="https://hollowknight.fandom.com/wiki/Wayward_Compass" target="_blank">
+	  						<img src="https://vignette.wikia.nocookie.net/hollowknight/images/7/7d/Wayward_Compass.png/revision/latest?cb=20180909165457" class="grow App-charms"/>
+						</a>
+						<a href="https://hollowknight.fandom.com/wiki/Wayward_Compass" class="center" target="_blank">
+		  					<text class="grow f7">Wayward<br/>Compass</text>
+						</a>
+						<a href="https://hollowknight.fandom.com/wiki/Wayward_Compass" target="_blank">
+	  						<img src="https://vignette.wikia.nocookie.net/hollowknight/images/8/8a/Gathering_Swarm.png/revision/latest?cb=20180909165419" class="grow App-charms"/>
+						</a>
+						<a href="https://hollowknight.fandom.com/wiki/Gathering_Swarm" class="center" target="_blank">
+		  					<text class="grow f7">Gathering<br/>Swarm</text>
+						</a>
+					</div>
+				</div>
+      		</body>
+      		
+    	</div>
+  	);
 }
 
 export default App;
